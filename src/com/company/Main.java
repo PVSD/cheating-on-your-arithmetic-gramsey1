32a9710f8845233e153ccb9e501253f3e6026f47
gramsey1 <graceramsey312@yahoo.com>
2018-09-05 11:15:33 -0400

    easy bro

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int ans = 79 + 3 * (4 + 82 - 68) - 7 +19;
+        int ans2 = (179 + 21 + 10) / 7 + 181;
+        int ans3 = 10389 * 56 * 11 + 2246;
+        System.out.println("79 + 3 * (4 + 82 - 68) - 7 +19 = " + ans);
+        System.out.println("(179 + 21 + 10) / 7 + 181) = " + ans2);
+        System.out.println("10389 * 56 * 11 + 2246 = " + ans3);
     }
 }
